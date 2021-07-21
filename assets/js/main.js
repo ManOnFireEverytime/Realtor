ff721f42bb38e4845dc4a1fd7baba925029ef109
Mofe Ogunkanmi <55137858+ManOnFireEverytime@users.noreply.github.com>
2021-07-21 17:50:43 +0100

    preloader

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,174 +1,287 @@
 /*==================== SHOW MENU ====================*/
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-if(navToggle){
-    navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
-    })
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu')
-    })
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 
 /*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
-
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
-function scrollHeader(){
-    const header = document.getElementById('header')
-    // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+function scrollHeader() {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 100) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
 }
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener("scroll", scrollHeader);
 
 /*==================== SWIPER DISCOVER ====================*/
 let swiper = new Swiper(".discover__container", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    loop: true,
-    spaceBetween: 32,
-    coverflowEffect: {
-        rotate: 0,
-    },
-})
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+  spaceBetween: 32,
+  coverflowEffect: {
+    rotate: 0,
+  },
+});
 
 /*==================== VIDEO ====================*/
-const videoFile = document.getElementById('video-file'),
-      videoButton = document.getElementById('video-button'),
-      videoIcon = document.getElementById('video-icon')
+const videoFile = document.getElementById("video-file"),
+  videoButton = document.getElementById("video-button"),
+  videoIcon = document.getElementById("video-icon");
 
-function playPause(){ 
-    if (videoFile.paused){
-        // Play video
-        videoFile.play()
-        // We change the icon
-        videoIcon.classList.add('ri-pause-line')
-        videoIcon.classList.remove('ri-play-line')
-    }
-    else {
-        // Pause video
-        videoFile.pause(); 
-        // We change the icon
-        videoIcon.classList.remove('ri-pause-line')
-        videoIcon.classList.add('ri-play-line')
-
-    }
+function playPause() {
+  if (videoFile.paused) {
+    // Play video
+    videoFile.play();
+    // We change the icon
+    videoIcon.classList.add("ri-pause-line");
+    videoIcon.classList.remove("ri-play-line");
+  } else {
+    // Pause video
+    videoFile.pause();
+    // We change the icon
+    videoIcon.classList.remove("ri-pause-line");
+    videoIcon.classList.add("ri-play-line");
+  }
 }
-videoButton.addEventListener('click', playPause)
+videoButton.addEventListener("click", playPause);
 
-function finalVideo(){
-    // Video ends, icon change
-    videoIcon.classList.remove('ri-pause-line')
-    videoIcon.classList.add('ri-play-line')
+function finalVideo() {
+  // Video ends, icon change
+  videoIcon.classList.remove("ri-pause-line");
+  videoIcon.classList.add("ri-play-line");
 }
 // ended, when the video ends
-videoFile.addEventListener('ended', finalVideo)
+videoFile.addEventListener("ended", finalVideo);
 
-
-/*==================== SHOW SCROLL UP ====================*/ 
-function scrollUp(){
-    const scrollUp = document.getElementById('scroll-up');
-    // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+/*==================== SHOW SCROLL UP ====================*/
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 200) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
 }
-window.addEventListener('scroll', scrollUp)
+window.addEventListener("scroll", scrollUp);
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-const sections = document.querySelectorAll('section[id]')
+const sections = document.querySelectorAll("section[id]");
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+function scrollActive() {
+  const scrollY = window.pageYOffset;
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
 }
-window.addEventListener('scroll', scrollActive)
+window.addEventListener("scroll", scrollActive);
 
 /*==================== SCROLL REVEAL ANIMATION ====================*/
 const sr = ScrollReveal({
-    distance: '60px',
-    duration: 2800,
-    // reset: true,
-})
+  distance: "60px",
+  duration: 2800,
+  // reset: true,
+});
 
-
-sr.reveal(`.home__data, .home__social-link, .home__info,
+sr.reveal(
+  `.home__data, .home__social-link, .home__info,
            .discover__container,
            .experience__data, .experience__overlay,
            .place__card,
            .sponsor__content,
-           .footer__data, .footer__rights`,{
-    origin: 'top',
+           .footer__data, .footer__rights`,
+  {
+    origin: "top",
     interval: 100,
-})
+  }
+);
 
-sr.reveal(`.about__data, 
+sr.reveal(
+  `.about__data, 
            .video__description,
-           .subscribe__description`,{
-    origin: 'left',
-})
+           .subscribe__description`,
+  {
+    origin: "left",
+  }
+);
 
-sr.reveal(`.about__img-overlay, 
+sr.reveal(
+  `.about__img-overlay, 
            .video__content,
-           .subscribe__form`,{
-    origin: 'right',
+           .subscribe__form`,
+  {
+    origin: "right",
     interval: 100,
-})
+  }
+);
 
-/*==================== DARK LIGHT THEME ====================*/ 
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'ri-sun-line'
+/*==================== CLOCK ====================*/
+const hour = document.getElementById("clock-hour"),
+  minutes = document.getElementById("clock-minutes"),
+  seconds = document.getElementById("clock-seconds");
+
+const clock = () => {
+  let date = new Date();
+
+  let hh = date.getHours() * 30,
+    mm = date.getMinutes() * 6,
+    ss = date.getSeconds() * 6;
+
+  // We add a rotation to the elements
+  hour.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+  minutes.style.transform = `rotateZ(${mm}deg)`;
+  seconds.style.transform = `rotateZ(${ss}deg)`;
+};
+setInterval(clock, 1000); // 1000 = 1s
+
+/*==================== CLOCK & DATE TEXT ====================*/
+const textHour = document.getElementById("text-hour"),
+  textMinutes = document.getElementById("text-minutes"),
+  textAmPm = document.getElementById("text-ampm"),
+  //   dateWeek = document.getElementById('date-day-week'),
+  dateDay = document.getElementById("date-day"),
+  dateMonth = document.getElementById("date-month"),
+  dateYear = document.getElementById("date-year");
+
+const clockText = () => {
+  let date = new Date();
+
+  let hh = date.getHours(),
+    ampm,
+    mm = date.getMinutes(),
+    day = date.getDate(),
+    // dayweek = date.getDay(),
+    month = date.getMonth(),
+    year = date.getFullYear();
+
+  // We change the hours from 24 to 12 hours and establish whether it is AM or PM
+  if (hh >= 12) {
+    hh = hh - 12;
+    ampm = "PM";
+  } else {
+    ampm = "AM";
+  }
+
+  // We detect when it's 0 AM and transform to 12 AM
+  if (hh == 0) {
+    hh = 12;
+  }
+
+  // Show a zero before hours
+  if (hh < 10) {
+    hh = `0${hh}`;
+  }
+
+  // Show time
+  textHour.innerHTML = `${hh}:`;
+
+  // Show a zero before the minutes
+  if (mm < 10) {
+    mm = `0${mm}`;
+  }
+
+  // Show minutes
+  textMinutes.innerHTML = mm;
+
+  // Show am or pm
+  textAmPm.innerHTML = ampm;
+
+  // If you want to show the name of the day of the week
+  // let week = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat']
+
+  // We get the months of the year and show it
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  // We show the day, the month and the year
+  dateDay.innerHTML = day;
+  // dateWeek.innerHTML = `${week[dayweek]}`
+  dateMonth.innerHTML = `${months[month]},`;
+  dateYear.innerHTML = year;
+};
+setInterval(clockText, 1000); // 1000 = 1s
+/*==================== DARK LIGHT THEME ====================*/
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
 
 // Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
 
 // We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
+    iconTheme
+  );
 }
 
 // Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
-})
+themeButton.addEventListener("click", () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
